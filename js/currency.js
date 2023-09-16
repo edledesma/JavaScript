cfe061b936a9101186b7630b40a9d5627a88e817
David Ledesma <ledesma@outlook.com.ar>
2023-09-16 08:34:48 -0300

    Updated API and cleaned code.

--- js/currency.js
+++ js/currency.js
@@ -1,45 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+  getPrices();
+});
+
 async function getPrices() {
-  const response = await fetch(
-    `https://www.dolarsi.com/api/api.php?type=valoresprincipales`
-  );
-  var data = await response.json();
+  try{
+  const responseO = await fetch("https://dolarapi.com/v1/dolares/oficial");
+  const responseB = await fetch("https://dolarapi.com/v1/dolares/blue");
 
-  const userInput = document.querySelector('#userInput');
-  const userInputPesos = document.querySelector('#userInputPesos');
+  const dataO = await responseO.json();
+  const dataB = await responseB.json();
 
-  const dolOfiB = data[0].casa.compra.replace(",",".");
-  const dolOfiS = data[0].casa.venta.replace(",",".");
-  const dolBlueB = data[1].casa.compra.replace(",",".");
-  const dolBlueS = data[1].casa.venta.replace(",",".");
+  console.log(dataO)
 
+  const userInput = document.querySelector("#userInput");
+  const userInputPesos = document.querySelector("#userInputPesos");
   const officialDollar = document.querySelector("#officialDollar");
-  officialDollar.innerHTML = `<p>Official Dollar: <br> Buy: $${dolOfiB} <br> Sell: $${dolOfiS} </p>`;
+  const lastUpdate= document.querySelector("#last-update");
 
+  timeUpdate = `Updated: `+(dataO.fechaActualizacion.replace('T',' / ').slice(0, -5));
+  lastUpdate.innerHTML = timeUpdate;
+
+  officialDollar.innerHTML = `<p>Official Dollar: <br> Buy: $${dataO.compra} <br> Sell: $${dataO.venta} </p>`;
   const bluelDollar = document.querySelector("#blueDollar");
-  bluelDollar.innerHTML = `<p>Blue Dollar: <br> Buy: $${dolBlueB} <br> Sell: $${dolBlueS} </p>`;
+  bluelDollar.innerHTML = `<p>Blue Dollar: <br> Buy: $${dataB.compra} <br> Sell: $${dataB.venta} </p>`;
 
-  console.log(dolOfiB)
-
-  const inputButton = document.querySelector('#inputButton');
+  const inputButton = document.querySelector("#inputButton");
   inputButton.addEventListener("click", function () {
-    calcDol(dolOfiB,dolOfiS,dolBlueB,dolBlueS);
-  })
-
-  const inputButtonP = document.querySelector('#inputButtonP');
+    calcDol();
+  });
+  const inputButtonP = document.querySelector("#inputButtonP");
   inputButtonP.addEventListener("click", function () {
-    calcPesos(dolOfiS,dolBlueS);
-  })
+    calcPesos();
+  });
+
+  function calcDol() {
+    const inputValue = parseFloat(document.querySelector("#dollarInput").value);
+    userInput.innerHTML = `<p><img src="./img/dollar.svg" width="32px"><br> Oficial Dollar Buy: ARS $${
+      inputValue * dataO.compra
+    }<br> Oficial Dollar Sell: ARS $${
+      inputValue * dataO.venta
+    }<br><img src="./img/dollarB.svg" width="32px"><br>Blue Dollar Buy: ARS $${
+      inputValue * dataB.compra
+    }<br> Blue Dollar Sell: ARS $${inputValue * dataB.venta} </p>`;
+  }
+
+  function calcPesos() {
+    const inputValuePesos = parseFloat(
+      document.querySelector("#pesosInput").value
+    );
+    const ofiPesos = parseFloat(inputValuePesos / dataO.venta).toFixed(2);
+    const bluePesos = parseFloat(inputValuePesos / dataB.venta).toFixed(2);
+    userInputPesos.innerHTML = `<p><img src="./img/dollar.svg" width="32px"><br> Oficial Dollar: USD $${ofiPesos}<br><img src="./img/dollarB.svg" width="32px"><br> Blue Dollar: USD $${bluePesos} </p>`;
+  }
+}catch (error) {
+  officialDollar.innerHTML = `<p>API is not reachable at this time.</p>`;
+  blueDollar.innerHTML = `<p>API is not reachable at this time.</p>`;
+  console.error("API is not reachable.", error);
 }
-
-function calcDol(dolOfiB,dolOfiS,dolBlueB,dolBlueS) {
-  const inputValue = parseFloat((document.querySelector('#dollarInput').value));
-
-  userInput.innerHTML = `<p><img src="./img/dollar.svg" width="32px"><br> Oficial Dollar Buy: ARS $${inputValue*dolOfiB}<br> Oficial Dollar Sell: ARS $${inputValue*dolOfiS}<br><img src="./img/dollarB.svg" width="32px"><br>Blue Dollar Buy: ARS $${inputValue*dolBlueB}<br> Blue Dollar Sell: ARS $${inputValue*dolBlueS} </p>`;
-}
-
-function calcPesos(dolOfiS,dolBlueS) {
-  const inputValuePesos = parseFloat((document.querySelector('#pesosInput').value));
-  const ofiPesos = parseFloat(inputValuePesos/dolOfiS).toFixed(2);
-  const bluePesos = parseFloat(inputValuePesos/dolBlueS).toFixed(2);
-  userInputPesos.innerHTML = `<p><img src="./img/dollar.svg" width="32px"><br> Oficial Dollar: USD $${ofiPesos}<br><img src="./img/dollarB.svg" width="32px"><br> Blue Dollar: USD $${bluePesos} </p>`;
 }
