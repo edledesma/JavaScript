aa176c55d3c4842f0ce877f90e551e7b7e6cc2bb
edledesma <100038680+edledesma@users.noreply.github.com>
2024-02-11 04:56:25 -0300

    stylized title

--- js/headFooter.js
+++ js/headFooter.js
@@ -1,12 +1,3 @@
-let footerCONT = `
-<div id="footer" class="bg-white">
-    <p>©EDLedesma - Thanks for reading - <a href="https://www.github.com/EDLedesma"><img src="img/github.svg" width="15px" alt=""/> GitHub</a> <a href="https://www.LinkedIn.com/in/edledesma"><img src="img/linkeding.svg" height="25px"/>LinkedIn</a>
-</p>
-</div>
-`;
-
-document.querySelector(".main-footer").innerHTML = footerCONT;
-
 let headerCONT = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div class="container-fluid">
   <a class="navbar-brand" href="index.html"
@@ -49,4 +40,12 @@ let headerCONT = `<nav class="navbar navbar-expand-lg bg-body-tertiary">
 
 document.querySelector(".main-header").innerHTML = headerCONT;
 
+let footerCONT = `
+<div id="footer" class="bg-white">
+    <p>©EDLedesma - Thanks for reading - <a href="https://www.github.com/EDLedesma"><img src="img/github.svg" width="15px" alt=""/> GitHub</a> <a href="https://www.LinkedIn.com/in/edledesma"><img src="img/linkeding.svg" height="25px"/>LinkedIn</a>
+</p>
+</div>
+`;
+
+document.querySelector(".main-footer").innerHTML = footerCONT;
 
